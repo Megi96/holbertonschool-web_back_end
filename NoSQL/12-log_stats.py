7019048d665389933d26bd057fe4071173f99658
Megi96 <xibrrakumegi@gmail.com>
2025-11-22 19:43:39 +0000

    Task nr 12 , log stats nginx

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,18 +11,18 @@ def log_stats():
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    
+
     # Total number of logs
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
-    
+
     # Methods stats
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-    
+
     # Status check (method=GET and path=/status)
     status_check = nginx_collection.count_documents({
         "method": "GET",
